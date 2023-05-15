9f91847f73d6d83b411315c0c31bede45baa7d11
dicksont2 <thomas.dickson@mediahubaustralia.com.au>
2023-05-15 13:58:07 +1000

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,40 +1,28 @@
 import React from 'react'
-import styled from 'styled-components';
-import { NavLink} from 'react-router-dom'
 import { Link } from 'react-scroll'
 
 import { Box, Button, ButtonGroup, Container, HStack, Heading } from '@chakra-ui/react';
 import { IoIosArrowBack } from 'react-icons/io'
 
-const Icon = styled(NavLink)`
-  color: #41e2ba;
-
-  /* Hover animation */
-    &:hover {
-        transition: 0.4s ease-in-out;
-        transform: scale(1.1);
-  }
-`
-
 function Navbar(props) {
   let menu;
-  const showButtons = props.buttons;
+  let showButtons = props.buttons;
 
-  if(showButtons) {
-    menu = (
-      <ButtonGroup>
-        <Button as={Link} to="about-wrapper" spy={true} smooth={true} cursor='pointer'>About Me</Button>
-        <Button as={Link} to="projects-wrapper" spy={true} smooth={true} cursor='pointer'>Projects</Button>
-        <Button as={Link} to="contact-wrapper" spy={true} smooth={true} cursor='pointer'>Hire Me</Button>
-      </ButtonGroup>
-    )
-  } else {
-    menu = (
-      <Icon to="/">
-        <IoIosArrowBack size={40}/>
-      </Icon>
-    )
-  }
+    if(showButtons) {
+      menu = (
+        <ButtonGroup>
+          <Button as={Link} to="about-wrapper" spy={true} smooth={true} cursor='pointer'>About Me</Button>
+          <Button as={Link} to="projects-wrapper" spy={true} smooth={true} cursor='pointer'>Projects</Button>
+          <Button as={Link} to="contact-wrapper" spy={true} smooth={true} cursor='pointer'>Hire Me</Button>
+        </ButtonGroup>
+      )
+    } else {
+      menu = (
+        <Icon to="/">
+          <IoIosArrowBack size={40}/>
+        </Icon>
+      )
+    }
   return (
     <Box as="nav" p={4} boxShadow="sm" position="fixed" w='100%' backgroundColor="white" zIndex='999'>
       <HStack spacing="10" justify="space-between">
